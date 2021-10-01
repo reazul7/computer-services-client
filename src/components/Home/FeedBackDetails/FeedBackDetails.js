6d555eca5d98171afded7ee4224cf1e190b87230
reazul7 <islamreazul7777@gmail.com>
2021-10-01 18:45:23 +0600

    feedback img design chnage

--- src/components/Home/FeedBackDetails/FeedBackDetails.js
+++ src/components/Home/FeedBackDetails/FeedBackDetails.js
@@ -3,12 +3,12 @@ const FeedBackDetails = ({ feedBack }) => {
     <div className="row mx-auto text-center my-3">
       {/* img-are */}
       {/* <img className="img-fluid mx-auto" style={{width: '170px', height: '150px', borderRadius: '50%'}} src={feedBack.newFile} alt=""/> */}
-      <div class="flex flex-wrap justify-center">
-        <div class="w-6/12 sm:w-4/12 px-2">
+      <div className="flex flex-wrap justify-center">
+        <div className="w-6/12 sm:w-4/12 px-2">
           <img
             src={feedBack.newFile}
-            alt="..."
-            class="shadow-lg rounded-bl-3xl rounded-tr-3xl w-full h-auto align-middle border-none"
+            alt="image loading..."
+            className="shadow-lg rounded-bl-3xl rounded-tr-3xl w-full h-auto align-middle border-none"
           />
         </div>
       </div>
