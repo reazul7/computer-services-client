25e967570e121bf0857b31a393dbd579c108c937
reazul7 <islamreazul7777@gmail.com>
2021-10-01 18:30:34 +0600

    feedback img design chnage

--- src/components/Home/FeedBackDetails/FeedBackDetails.js
+++ src/components/Home/FeedBackDetails/FeedBackDetails.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const FeedBackDetails = ({feedBack}) => {
-    const description = feedBack.description;
-    const newDescription = description.split(' ').slice(0, 14).join(' ');
-    return (
-        <div className="row mx-auto text-center my-3">
-            <img className="img-fluid mx-auto w-40 h-40 rounded-full" src={feedBack.newFile} alt=""/>
-            {/* style={{width: '170px', height: '150px', borderRadius: '50%'}} */}
-            <h6 className="font-bold text-lg mt-2 w-full">{feedBack.name}</h6>
-            <h5 className="text-secondary py-1 uppercase ">{feedBack.designation}</h5>
-            <h5 className="text-dark">" {newDescription} "</h5>
+const FeedBackDetails = ({ feedBack }) => {
+  const description = feedBack.description;
+  const newDescription = description.split(" ").slice(0, 14).join(" ");
+  return (
+    <div className="row mx-auto text-center my-3">
+      {/* img-are */}
+      {/* <img className="img-fluid mx-auto" style={{width: '170px', height: '150px', borderRadius: '50%'}} src={feedBack.newFile} alt=""/> */}
+      <div class="flex flex-wrap justify-center">
+        <div class="w-6/12 sm:w-4/12 px-2">
+          <img
+            src={feedBack.newFile}
+            alt="..."
+            class="shadow-lg rounded-bl-3xl rounded-tr-3xl w-full h-auto align-middle border-none"
+          />
         </div>
-    );
+      </div>
+
+      {/* text-area */}
+      <h6 className="font-bold text-lg mt-2 w-full">{feedBack.name}</h6>
+      <h5 className="text-secondary py-1 uppercase ">{feedBack.designation}</h5>
+      <h5 className="text-dark">" {newDescription} "</h5>
+    </div>
+  );
 };
 
 export default FeedBackDetails;
